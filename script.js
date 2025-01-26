560109b1b8d912fb3556aaa785b64664031142cb
Monu Nigaam <monunigaam1658@gamil.com>
2025-01-26 20:39:17 +0530

    css completed

---
+++ script.js
@@ -0,0 +1,8 @@
+const colorPicker=document.getElementById("colorPicker");
+const canvaColor=document.getElementById('canvaColor');
+const fontSize=document.getElementById('fontSize');
+const canva=document.getElementById('myCanvas');
+const clearButton=document.getElementsByName("clearButton");
+const saveButton=document.getElementById("saveButton");
+const retrieveButton=document.getElementById("retrieveButton");
+
